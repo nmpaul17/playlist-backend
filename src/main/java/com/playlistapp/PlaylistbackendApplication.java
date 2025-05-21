8384674b90d73c4c74578c920e13f45faf101e73
nmpaul17 <nihalpaul17@gmail.com>
2025-05-21 00:09:07 -0500

    Finish backend + PostgreSQL setup

---
+++ src/main/java/com/playlistapp/PlaylistbackendApplication.java
@@ -0,0 +1,13 @@
+package com.playlistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistbackendApplication.class, args);
+	}
+
+}
